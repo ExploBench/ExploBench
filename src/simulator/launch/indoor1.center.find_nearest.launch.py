01c1e25cd36db6b80552a1b1853794362e8a7b99
TangJingfan <tangjingfan@gmail.com>
2025-12-03 22:32:42 +0800

    feat: update simulator

---
+++ src/simulator/launch/indoor1.center.find_nearest.launch.py
@@ -0,0 +1,57 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+from datetime import datetime
+
+def generate_launch_description():
+    simulator_share = get_package_share_directory('simulator')
+    essential_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(simulator_share, 'launch', 'indoor1.center.sim_essential.launch.py')
+        )
+    )
+    rviz_config_file = os.path.join(
+        simulator_share,
+        'rviz',
+        'mp3d_find_nearest.rviz'
+    )
+
+    return LaunchDescription(
+        [
+            essential_launch,
+            Node(
+                package="find_nearest",
+                executable="find_nearest_node",
+                name="find_nearest",
+                output="screen"
+            ),
+            Node(
+                package='rviz2',
+                executable='rviz2',
+                name='rviz',
+                output='screen',
+                arguments=['-d', rviz_config_file]
+            ),
+            Node(
+                package="exp_statistics",
+                executable="exp_statistics_node",
+                name="exp_statistics",
+                output="screen",
+                parameters=[
+                    {
+                        "stat_file_path": os.path.join(
+                        os.path.dirname(os.path.dirname((os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))))),
+                            "src",
+                            "simulator",
+                            "statistics",
+                            "indoor1",
+                            f"find_nearest_stats_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv",
+                        )
+                    }
+                ],
+            )
+        ]
+    )
---
+++ src/simulator/launch/indoor2.center.sim_essential.launch.py
@@ -0,0 +1,79 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+import math
+
+
+def generate_launch_description():
+    height = 12.5
+    resolution = 0.1
+
+    return LaunchDescription(
+        [
+            Node(
+                package="env_publisher",
+                executable="ply_publisher_node",
+                name="ply_publisher",
+                output="screen",
+                parameters=[
+                    {
+                        "pointcloud_file": "ply/indoor2.ply",
+                        "local_radius": 20.0,
+                        "scan_resolution": 10.0,
+                        "sample_step": 2.0,
+                        "x_offset": -4.2,
+                        "y_offset": 2.0,
+                        "scaler": 1.0,
+                    }
+                ],
+            ),
+            Node(
+                package="map_builder",
+                executable="map_builder_node",
+                name="map_builder",
+                output="screen",
+                parameters=[
+                    {
+                        "resolution": resolution,
+                        "size_x": 151.0,
+                        "size_y": 151.0,
+                        "height": height,
+                        "num_rays": 120.0,
+                        "max_range": 12.0,
+                    }
+                ],
+            ),
+            Node(
+                package="odom_manager",
+                executable="odom_manager_node",
+                name="odom_manager",
+                output="screen",
+                parameters=[
+                    {
+                        "av1/x_init": 8.0,  # 60
+                        "av1/y_init": 8.0,  # 20
+                        "av1/z_init": height,
+                        "av1/yaw": 5 * math.pi / 4,
+                        "resolution": resolution,
+                    }
+                ],
+            ),
+            Node(
+                package="local_planner",
+                executable="path_tracker_node",
+                name="path_tracker",
+                output="screen",
+                parameters=[
+                    {
+                        "robot_name": "av1",
+                        "kp_linear": 2.5,
+                        "kp_angular": 3.0,
+                        "kp_angular_small": 1.0,
+                        "tolerance": 0.05,
+                        "max_linear_velocity": 0.3,
+                        "max_angular_velocity": 1.0,
+                    }
+                ],
+            )
+        ]
+    )
